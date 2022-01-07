4cf0d38dce20d642dae8117fcbcadd70769b4029
SrKing00 <79137190+SrKing00@users.noreply.github.com>
2022-01-07 18:42:52 +0100

    idk what im doing

--- app/copy app.py
+++ app/copy app.py
@@ -5,6 +5,8 @@ import cv2
 import numpy as np
 import torch
 import albumentations as A
+#from sklearn.metrics import jaccard_score
+
 import segmentation_models_pytorch as smp
 
 # ---------------------------------#
@@ -61,9 +63,19 @@ def imgread_preprocessing(uploaded_img):  # final preprocessing function in stre
     sample = preprocessing(image=image)
     # image, mask = sample['image'], sample['mask']
     image = sample['image']
-
     return image
 
+@st.cache(allow_output_mutation=True)
+def mask_read_local(gt_mask_dir):
+    gt_mask = cv2.imread(gt_mask_dir, cv2.IMREAD_GRAYSCALE)
+    gt_mask = cv2.threshold(gt_mask, 0, 255, cv2.THRESH_BINARY)[1]
+    return gt_mask
+
+@st.cache(allow_output_mutation=True)
+def mask_read_uploaded(gt_mask_dir):
+    gt_mask = cv2.imread(gt_mask_dir, cv2.IMREAD_GRAYSCALE)
+    gt_mask = cv2.threshold(gt_mask, 0, 255, cv2.THRESH_BINARY)[1]
+    return gt_mask
 
 ARCHITECTURE = smp.DeepLabV3Plus
 BACKBONE = 'efficientnet-b3'
@@ -179,7 +191,7 @@ if uploaded_file is None:
             index = 1,
         )
         if pre_trained_img != "None":
-            selected_img = "./src/data/test/" + pre_trained_img
+            selected_img_dir = "./src/data/test/" + pre_trained_img
 
 
 else:
@@ -204,6 +216,32 @@ st.sidebar.markdown("""
 n_classes = 1
 activation = 'sigmoid'
 
+def extract_bboxes(mask):
+    """Compute bounding boxes from masks.
+    mask: [height, width, num_instances]. Mask pixels are either 1 or 0.
+
+    Returns: bbox array [num_instances, (y1, x1, y2, x2)].
+    """
+    boxes = np.zeros([mask.shape[-1], 4], dtype=np.int32)
+    for i in range(mask.shape[-1]):
+        m = mask[:,:, i]
+        # Bounding box.
+        horizontal_indicies = np.where(np.any(m, axis=0))[0]
+        vertical_indicies = np.where(np.any(m, axis=1))[0]
+        if horizontal_indicies.shape[0]:
+            x1, x2 = horizontal_indicies[[0, -1]]
+            y1, y2 = vertical_indicies[[0, -1]]
+            # x2 and y2 should not be part of the box. Increment by 1.
+            x2 += 1
+            y2 += 1
+        else:
+            # No mask for this instance. Might happen due to
+            # resizing or cropping. Set bbox to zeros
+            x1, x2, y1, y2 = 0, 0, 0, 0
+        boxes[i] = np.array([y1, x1, y2, x2])
+    return boxes.astype(np.int32)
+
+
 
 def deploy1(uploaded_file):
     # create model
@@ -212,7 +250,7 @@ def deploy1(uploaded_file):
     # model = get_model(model_path)
 
     # st.write(uploaded_file)
-    col1, col2, col3 = st.columns((0.4, 0.4, 0.2))
+    col1, col2, col3, col4 = st.columns((0.4, 0.4, 0.3, 0.3))
 
     with col1:  # visualize
         st.subheader('1.Visualize Image')
@@ -233,7 +271,10 @@ def deploy1(uploaded_file):
             image = torch.from_numpy(img).to(DEVICE).unsqueeze(0)
             pr_mask = model.predict(image)
             pr_mask = (pr_mask.squeeze().numpy().round())
-
+            prob = model(image)
+            ann = np.argmax(prob)
+            print(prob)
+            print(ann)
             st.image(pr_mask, caption='Predicted Mask')
 
     with col3:
@@ -245,23 +286,24 @@ def deploy1(uploaded_file):
             **Coordinates**:...
             """
         )
+
     del model
     gc.collect()
 
 
-def deploy2(selected_img):
+def deploy2(selected_img_dir):
     # model = sm.Unet(BACKBONE, classes=n_classes, activation=activation)
     # model.load_weights(f'{model_path}')
 
     # model = get_model(ARCHITECTURE, BACKBONE, n_classes, activation)
     model = torch.load(f'{model_path}',map_location ='cpu')
 
-    col1, col2, col3 = st.columns((0.4, 0.4, 0.2))
+    col1, col2, col3, col4 = st.columns((0.6, 0.6, 0.6, 0.6))
 
     with col1:  # visualize
-        st.subheader('1.Visualize Image')
+        st.subheader('Visualize Image')
         with st.spinner(text="Loading the image..."):
-            selected_img = cv2.imread(selected_img)
+            selected_img = cv2.imread(selected_img_dir)
             image = cv2.resize(selected_img, (256, 256))
 
             st.image(
@@ -269,32 +311,42 @@ def deploy2(selected_img):
                 caption='Image Selected')
 
     with col2:  # classify
-        st.subheader('2. Model Prediction')
+        st.subheader('Model Prediction')
         with st.spinner(text="The model is running..."):
             img = imgread_preprocessing(selected_img)
             # image = np.expand_dims(img, axis=0)
             image = torch.from_numpy(img).to(DEVICE).unsqueeze(0)
             pr_mask = model.predict(image)
-            pr_mask = (pr_mask.squeeze().numpy().round())
+            gt_mask_dir = selected_img_dir.replace('.png','_label.png')
+            gt_mask = mask_read_local(gt_mask_dir)
+            print("GT",gt_mask.shape)
 
+            pr_mask = (pr_mask.squeeze().numpy().round())
+            print(gt_mask)
+            intersection = np.logical_and(pr_mask, gt_mask)
+            union = np.logical_or(pr_mask, gt_mask)
+            iou_score = np.round(np.sum(intersection) / np.sum(union),2)
+            print('IoU is %s' % iou_score)
             st.image(pr_mask, caption='Predicted Mask')
+            st.image(gt_mask, caption='Ground Truth Mask')
 
     with col3:
-        st.subheader('3. Related Data')
+        st.subheader('Model Performance')
+        st.write(f'**IoU score**: {iou_score}')
+        del model
+        gc.collect()
+
+    with col4:
+        st.subheader('Other information')
         st.write(
-            """
-            **Area predicted**:...
-
-            **Coordinates**:...
-            """
+                """
+                **Area predicted**:...
+            
+                **Coordinates**:...
+                """
         )
-
-    del model
-    gc.collect()
-
-
 if uploaded_file is not None:
     deploy1(uploaded_file)
 elif pre_trained_img != 'None':
-    deploy2(selected_img)
+    deploy2(selected_img_dir)
 
